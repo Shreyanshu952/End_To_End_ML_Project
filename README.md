6fa61078558d7782b644d99123235caeab830843
Shreyanshu952 <shreyanshupachauri0515@gmail.com>
2023-12-01 23:29:13 +0530

    experiments has been launched to DAGshub

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # This is my Machine Learning End to End Project
+
+MLFLOW_TRACKING_URI=https://dagshub.com/Shreyanshu952/End_To_End_ML_Project.mlflow \
+MLFLOW_TRACKING_USERNAME=Shreyanshu952 \
+MLFLOW_TRACKING_PASSWORD=e344a5dcf7202d9ed9fc310bdae64acda2b2d18b \
--- src/DimondPricePrediction/components/model_evaluation.py
+++ src/DimondPricePrediction/components/model_evaluation.py
@@ -43,6 +43,20 @@ class ModelEvaluation:
             model = load_object(model_path)
             logging.info("Best Model has been loaded")
 
+            # mlflow keeps the track of experiments and also do model registry
+            # if we use directly mlflow without the use of DAGshub then our code will be tracked only in local enviroment 
+            # and we get ui which we can run only through local server and and we can not track our experiments using outside servers
+            # for doing that, we have to launch our experiments on DAGshub
+            # DAGshub can host our experiments and also register our model which can we seen to outside world
+            # DAGshub is an open source platform
+
+            #writing code for setting local environment to DAGshub
+            mlflow.set_registry_uri("https://dagshub.com/Shreyanshu952/End_To_End_ML_Project.mlflow")
+            
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            logging.info(f"Type of store is {tracking_url_type_store}")
+
             #initiating mlflow run for tracking metrices
             with mlflow.start_run():
                 #predicting the X_test values using our best model
@@ -55,6 +69,27 @@ class ModelEvaluation:
                 mlflow.log_metric("rmse", rmse)
                 mlflow.log_metric("r2_score", rsquared_value)
 
+                # model registry does not work with file store
+                # hence setting if else condition
+                if tracking_url_type_store != "file":
+                    #register the model
+                    mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
+
+                else:
+                    mlflow.sklearn.log_model(model, "model")
+
+                # before executing the training pipeline for experiment tracking and model registry
+                # run these below code on terminal
+                """
+                export MLFLOW_TRACKING_URI=https://dagshub.com/Shreyanshu952/End_To_End_ML_Project.mlflow
+                export MLFLOW_TRACKING_USERNAME=Shreyanshu952
+                export MLFLOW_TRACKING_PASSWORD=e344a5dcf7202d9ed9fc310bdae64acda2b2d18b
+                """
+                # These commands are configuring MLflow to log and retrieve experiment information from the specified Dagshub tracking
+                # server, and they include authentication credentials (username and password) for secure access to the tracking server.
+
+                logging.info("Model Evaluation is done successfully")
+                logging.info("Tracking Experiments and Model registry is successfully done")
         except Exception as e:
             logging.info("Error occured at model evaluation stage")
             raise customexception(e, sys)
